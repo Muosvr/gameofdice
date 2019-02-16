669593255ba2c57578e9e7e7ab31244d50963c5b
Muosvr <lujason2015@gmail.com>
2019-02-16 15:34:21 -0500

    websocket setup

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const users = require("./routes/api/users");
 const bodyParser = require("body-parser");
 const app = express();
 
+// Add websocket
+var expressWs = require("express-ws")(app);
+
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -22,6 +25,12 @@ app.get("/", (req, res) => res.send("Hello"));
 
 app.use("/api/score", score);
 app.use("/api/users", users);
+
+// Test websocket
+app.ws("/echo", (ws, req) => {
+  ws.on("message", msg => ws.send(msg));
+  console.log("socket", req.testing);
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
